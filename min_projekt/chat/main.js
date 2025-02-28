fe07761e48d39bd75e14d36f59b027ea2e8d4300
AshurW <ashurw@gmail.com>
2025-02-28 11:21:03 +0100

    Add bonus

--- min_projekt/chat/main.js
+++ min_projekt/chat/main.js
@@ -1,14 +1,26 @@
+const chatContainer = document.getElementById('chat-container')
 const messageForm = document.getElementById("message-form")
 const messageInput = document.getElementById("message-input")
 const messageContainer = document.getElementById("message-container")
 
-const messageArray = []
+const loginContainer = document.getElementById('login-container')
+const loginForm = document.getElementById('login-form')
+const usernameInput = document.getElementById('username-input')
+
+let username = ""
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    username = usernameInput.value
+    loginContainer.classList.add('hidden')
+    chatContainer.classList.remove('hidden')
+})
 
 function createMessage(message) {
     const messageDiv = document.createElement('div')
     messageDiv.className = "message"
     const messageP = document.createElement('p')
-    messageP.textContent = message
+    messageP.textContent = `${username}: ${message}`
     messageDiv.appendChild(messageP)
     return messageDiv
 }
